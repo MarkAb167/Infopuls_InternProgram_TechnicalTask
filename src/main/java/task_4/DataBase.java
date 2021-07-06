0795a113ee19583d96bb6ada8a751a5be7252062
Viktor <lebedevit4@gmail.com>
2021-07-06 13:47:45 +0300

    Add SQL request for task4.

--- src/main/java/task_4/DataBase.java
+++ src/main/java/task_4/DataBase.java
@@ -3,12 +3,19 @@ package task_4;
 public class DataBase {
 
     public String getFirstRequest(){
-        return "SELECT account_id, customer_id, avail_balance" +
-                "FROM account JOIN customer" +
-                "WHERE account.status = 'ACTIVE', account.avail_balance > 1000";
+        return "SELECT account_id, cust_id, avail_balance" +
+                "FROM account" +
+                "WHERE status = 'ACTIVE' " +
+                "AND avail_balance > 1000 ";
+
     }
 
     public String getSecondRequest(){
-        return "";
+        return "SELECT account.account_id, customer.fed_id, product.name" +
+                "FROM customer AS c JOIN account AS a" +
+                "ON c.customer_id = a.cust_id" +
+                "WHERE c.cust_type_cd = 'I'" +
+                "JOIN product AS p" +
+                "ON a.product_cd = p.product_cd ";
     }
 }
